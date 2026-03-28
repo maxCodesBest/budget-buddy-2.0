24565578546da5fc03b6e40331b454822d657525
maxcodesbest <maxcodesbest@gmail.com>
2026-03-28 14:36:17 +0300

    try fix toml

--- budget-buddy-2.0-backend/src/main.ts
+++ budget-buddy-2.0-backend/src/main.ts
@@ -28,6 +28,7 @@ async function bootstrap() {
       forbidUnknownValues: false,
     }),
   );
-  await app.listen(3000);
+  const port = Number(config.get('PORT') ?? 3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
